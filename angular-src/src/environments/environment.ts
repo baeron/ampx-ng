7504559e11bd7fc636776f7d75684dd0cf0f0b9d
Ievgen <baranoffei@gmail.com>
2018-11-08 14:49:35 +0200

    Fix commit3

--- angular-src/src/environments/environment.ts
+++ angular-src/src/environments/environment.ts
@@ -3,9 +3,10 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// 'http://localhost:3000/',
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: '/',
   sadmin: 'superAdmin@ampx.ca',
   admin: 'admin@ampx.ca'
 };
